b2eebde5653ca671fed6a746b69b8b3109708222
Floriane TUERNAL-SABOTINOV <ftuernal@made-f0Br7s18.clusters.42paris.fr>
2023-11-06 17:08:23 +0100

    00_00

---
+++ cpp00/ex00/Megaphone.class.hpp
@@ -0,0 +1,18 @@
+#ifndef MEGAPHONE_CLASS_HPP
+# define MEGAPHONE_CLASS_HPP
+
+# include <string>
+# include <iostream>
+# include <cctype>
+
+class Megaphone {
+  std::string MakeNoise(const std::string& msg){
+    std::string res;
+    std::string::iterator i = str.begin();
+
+    for (i
+
+  }
+};
+
+#endif
---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cctype>
+
+int main(int ac, char **av){
+  if (ac == 1){
+
+  }
+  else {
+
+    }
+  return (0);
+}
