5a55a55b04c8818d97972e49441bfb3aa91f65e8
Floriane TUERNAL-SABOTINOV <ftuernal@made-f0Br6s18.clusters.42paris.fr>
2023-11-07 16:25:07 +0100

    working on ex01

---
+++ cpp00/ex01/inc/Phonebook.hpp
@@ -0,0 +1,20 @@
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+# include <iostream>
+# include <string>
+# include <>
+
+class PhoneBook {
+
+public:
+
+private:
+  
+};
+
+class Contact {
+
+}
+#endif // !PHONEBOOK_HPP
+
---
+++ cpp00/ex01/srcs/main.cpp
@@ -0,0 +1,10 @@
+#include "Phonebook.hpp"
+
+int main(void) {
+  std::string buf[512];
+
+  while (1) {
+    std::cin >> buf;
+    if ()
+  }
+}
