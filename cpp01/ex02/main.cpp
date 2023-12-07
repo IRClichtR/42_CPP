b2d617de151a95ae0bab0840205a7ceb39d540a4
Floriane TUERNAL-SABOTINOV <ftuertinov@proton.me>
2023-12-07 16:03:49 +0100

    ex02

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,30 @@
+# include <iostream>
+# include <string>
+
+# define BLUE   "\033[0;34m"
+# define RESET   "\033[0m"
+
+int  main(void) {
+
+  std::string   myString = "HI THIS IS BRAIN";
+  std::string*  stringPTR = &myString;
+  std::string&  stringREF = myString;
+
+
+  //display address of string;
+  std::cout << BLUE << "address of the string: " << RESET << &myString << std::endl;
+  //display address stored in stringPTR
+  std::cout << BLUE << "address stored in stringPTR: " << RESET << stringPTR << std::endl;
+  //display address stored in stringREF;
+  std::cout << BLUE << "address stored in stringREF: " << RESET << &stringREF << std::endl;
+  
+  std::cout << std::endl;
+  std::cout << std::endl;
+
+  //display value of string through stringPTR
+  std::cout << BLUE << "value of string through stringPTR: " << RESET << *stringPTR << std::endl;
+  //display value pointed by stringREF
+  std::cout << BLUE << "value pointed by stringREF: " << RESET << stringREF << std::endl;
+
+  return (0);
+}
