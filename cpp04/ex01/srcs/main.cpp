f561729729c12336eed8ba3f9ab228c48881cd9d
Irrlichter <ftuertinov@proton.me>
2024-02-16 17:20:40 +0100

    WIP

--- cpp04/ex01/srcs/main.cpp
+++ cpp04/ex01/srcs/main.cpp
@@ -1,54 +1,62 @@
 #include "define.hpp"
 
 int main() {
-
-  Animal** test = new Animal*[6];
-
-  for (int i = 0; i < 6/2; i++) {
-    test[i] = new Cat();
-    std::cout << "[" << i << "] ";
-    test[i]->makeSound();
-    std::cout << std::endl;
-  }
-
-  for (int i = 3; i < 6; i++) {
-    test[i] = new Dog();
-    std::cout << "[" << i << "] ";
-    test[i]->makeSound();
-    std::cout << std::endl;
-  }
-
-  std::cout << RED << " ___ TEST1: Erase all Animal tab ___ " << RESET << std::endl;
-  for (int i = 0 ; i < 6 ; i++)
-    delete test[i];
-  delete [] test;
-  std::cout << RED << " ___ TEST1: Done! ___ " << RESET << std::endl;
+  //
+  // Animal** test = new Animal*[6];
+  //
+  // for (int i = 0; i < 6/2; i++) {
+  //   test[i] = new Cat();
+  //   std::cout << "[" << i << "] ";
+  //   test[i]->makeSound();
+  //   std::cout << std::endl;
+  // }
+  //
+  // for (int i = 3; i < 6; i++) {
+  //   test[i] = new Dog();
+  //   std::cout << "[" << i << "] ";
+  //   test[i]->makeSound();
+  //   std::cout << std::endl;
+  // }
+  //
+  // std::cout << RED << " ___ TEST1: Erase all Animal tab ___ " << RESET << std::endl;
+  // for (int i = 0 ; i < 6 ; i++)
+  //   delete test[i];
+  // delete [] test;
+  // std::cout << RED << " ___ TEST1: Done! ___ " << RESET << std::endl;
   std::cout << std::endl;
 
-  std::cout << RED << " ___ TEST2: persistence of copyCat and copyDog __ " << RESET << std::endl;
+  std::cout << RED << " ___ TEST2: persistence of copyCat __ " << RESET << std::endl;
 
   Cat* C = new Cat;
-  Dog* D = new Dog;
+  std::cout << "Cat adress = " << C << std::endl;
 
   Cat* copyCat = new Cat(*C);
-  Dog* copyDog = new Dog(*D);
-
-  std::cout << "Cat adress = " << C << std::endl;
   std::cout << "copyCat adress = " << copyCat << std::endl;
-  std::cout << "Dog adress = " << D << std::endl;
-  std::cout << "copyDog adress = " << copyDog << std::endl;
-  std::cout << RED << " ___ Delete Cat and Dog ___ " << RESET << std::endl;
+  std::cout << RED << " ___ Delete Cat ___ " << RESET << std::endl;
   delete C;
-  delete D;
 
   std::cout << RED << " ___ Done! ___ " << RESET << std::endl;
 
   std::cout << "copyCat adress = " << copyCat << std::endl;
-  std::cout << "copyDog adress = " << copyDog << std::endl;
-
   std::cout << RED << " ___ Delete Copies ___ " << RESET << std::endl;
   delete copyCat;
-  delete copyDog;
+
+
+  // Dog* D = new Dog;
+
+  // Dog* copyDog = new Dog(*D);
+
+  // std::cout << "Dog adress = " << D << std::endl;
+  // std::cout << "copyDog adress = " << copyDog << std::endl;
+
+  // delete D;
+
+
+  // std::cout << "copyDog adress = " << copyDog << std::endl;
+
+
+  // delete copyDog;
+  //
   std::cout << RED << " ___ Done! ___ " << RESET << std::endl;
   
   return (0);
