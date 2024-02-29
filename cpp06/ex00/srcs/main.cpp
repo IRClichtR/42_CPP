67ff50bb86c5d3803033df54a14b47717e0c13b3
Floriane TUERNAL-SABOTINOV <ftuertinov@proton.me>
2024-02-29 11:54:13 +0100

    WIP

--- cpp06/ex00/srcs/main.cpp
+++ cpp06/ex00/srcs/main.cpp
@@ -1,7 +1,19 @@
 #include "define.hpp"
 
-int main() {
+int main(int ac, char **av) {
 
-  std::cout << GREEN << "I'm OK !" << RESET << std::endl;
+  if (ac < 2) {
+
+    std::cout << RED << "Missing argument" << RESET << std::endl;
+    return (1);
+  }
+  else if (ac > 2) {
+
+    std::cout << RED << "Too many arguments" << RESET << std::endl;
+    return (1);
+  }
+
+  std::string literal = av[1];
+  ScalarConverter::convert(literal, av[1]);
   return (0);
 }
--- cpp06/ex00/srcs/utils.cpp
+++ cpp06/ex00/srcs/utils.cpp
@@ -4,7 +4,7 @@ int defineType(std::string& literal) {
 
   if (!isdigit(literal[0]) && literal.size() == 1)
     return (CHAR);
-  else if (literal.size() > 1 && literal[1] != 1 && !isdigit(literal[1]) && notNumberOrFloat(literal && notInf(literal))
+  else if (literal.size() > 1 && literal[1] != '.' && !isdigit(literal[1]) && notNumberOrFloat(literal) && notInf(literal))
       return (0);
   else {
     for (unsigned int i = 0; i < literal.size(); i++) {
@@ -22,7 +22,7 @@ bool notNumberOrFloat(std::string& literal) {
   
   std::string tab[2] = {"nan","nanf"};
 
-  for (int i = 0 ; i < tab.length() ; i++)
+  for (int i = 0 ; i < 2 ; i++)
     if (literal == tab[i])
       return (true);
   return (false);
@@ -32,13 +32,13 @@ bool notInf(std::string& literal) {
   
   std::string tab[6] = {"inf","inff","-inf", "-inff", "+inf","+inff"};
 
-  for (int i = 0 ; i < tab.length() ; i++)
+  for (int i = 0 ; i < 6 ; i++)
     if (literal == tab[i])
       return (true);
   return (false);
 }
 
-int getPrecision(literal) {
+int getPrecision(std::string& literal) {
 
   std::size_t position = literal.find(".");
   int precision = literal.size() - position - 1;
@@ -54,7 +54,7 @@ void  displayChar(double value, std::string& literal) {
     std::cout << RED << "char: impossible cast" << RESET << std::endl;
   else {
     char c = static_cast<char>(value);
-    if (!is_print(c)) 
+    if (!isprint(c)) 
       std::cout << RED << "char: not displayable" << RESET << std::endl;
     else 
       std::cout << "char: '" << c << "'" << std::endl;
@@ -80,7 +80,7 @@ void displayFloat(double value, int precision) {
 
 void displayDouble(double value, int precision) {
   
-  std::cout << "double: '" << std::fixed << std::setprecision(precision) << value << std::endl;
+  std::cout << "double: '" << std::fixed << std::setprecision(precision) << value << "'"<< std::endl;
 }
 
 void  isImpossible() {
