118dc03f9a0db915770912b0da274f1e5522003f
Floriane TUERNAL-SABOTINOV <ftuertinov@proton.me>
2024-03-06 11:46:23 +0100

    WIP

---
+++ cpp08/ex00/inc/define.hpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   define.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*By: ftuernal <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/05 16:53:58 by ftuernal          #+#    #+#             */
+/*   Updated: 2023/12/05 16:54:00 by ftuernal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DEFINE_HPP
+# define DEFINE_HPP
+
+//Include Headers______________________________________________________________/
+
+# include <iostream>
+
+extern bool DEBUG;
+
+
+
+//Utils Functions______________________________________________________________/
+
+
+int   defineType(std::string& literal);
+void  displayChar(double value, std::string& literal);
+void  displayDouble(double value, int precision);
+void  displayInt(double value,std::string& literal);
+void  displayFloat(double value, int precision);
+int   getPrecision(std::string& literal);
+bool  notInf(std::string& literal);
+bool  notNumberOrFloat(std::string& literal);
+void  isImpossible();
+
+//ANSI Colors__________________________________________________________________/
+
+# define RED    "\033[0;31m"  
+# define GREEN  "\033[0;32m"
+# define YELLOW "\033[0;33m"
+# define BLUE   "\033[0;34m"
+# define PURPLE "\033[0;35m"
+# define CYAN   "\033[0;36m"
+# define RESET  "\033[0m"
+
+
+
+#endif //define.hpp
